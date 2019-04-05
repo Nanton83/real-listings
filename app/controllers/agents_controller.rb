af2ce321c67dca894458246002646d777ed0c7b3
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-04-05 15:07:44 -0400

    working with login

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -17,9 +17,32 @@ class AgentsController < ApplicationController
         else
             @agent = Agent.create(email: params[:email], user_name: params[:user_name], password: params[:password])
             @agent.save
-            session[:user_id] = @agent.id
+            session[:id] = @agent.id
             erb :'agent/show'
         end
+    end
+
+    get '/login' do
+        #if !logged_in?
+        erb :'/agent/login'
+    end 
+
+    post '/login' do
+        @agent = Agent.find_by(:user_name => params[:username])
+        binding.pry
+        if @agent && @agent.authenticate(params[:password])
+
+            session[:id] = @agent.id
+            redirect to '/agent/show'
+        else
+            redirect to '/signup'
+        end
+    end
+
+    get '/logout' do
+        #if logged_in?
+        session.destroy
+        redirect to '/login'
     end
 
 end
