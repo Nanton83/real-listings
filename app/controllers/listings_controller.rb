bafce97cb6546826fa28e36a32f638a56626d3fb
nanton83 <nicholas.romans@gmail.com>
2019-04-23 15:16:26 -0400

    restful routes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,17 +7,21 @@ class ListingsController < ApplicationController
         erb :'/listings/listings'
     end
 
-    get '/create_listing' do
+    get '/listings/new' do
         erb :'listings/create_listing'
     end
 
-    post '/create_listing' do
+    post '/listings' do
         @listing = current_agent.listings.create(address: params[:address], bedrooms: params[:bedrooms], bathrooms: params[:bathrooms], square_feet: params[:square_feet], price: params[:price])
         if @listing.save
         redirect to "/agents/#{@listing.agent_id}"
         else
-        redirect to '/create_listing'
+        redirect to '/listings/new'
         end
+    end
+
+    get '/listings/:id' do
+      @listing = Listing.find_by(id: params[:id])
     end
 
     get '/listings/:id/edit' do
@@ -29,22 +33,13 @@ class ListingsController < ApplicationController
       end
     end
 
-    get '/listings/:id' do
-      @listing = Listing.find_by(id: params[:id])
-    end
-    
-    post '/listings/:id' do
-      
-    end
-
     patch '/listings/:id' do
       
       if logged_in?
         @listing = Listing.find_by(id: params[:id])
-        binding.pry
         if params[:address] && params[:bedrooms] && params[:bathrooms] && params[:square_feet] && params[:price]
         @listing.update(address: params[:address], bedrooms: params[:bedrooms], bathrooms: params[:bathrooms], square_feet: params[:square_feet], price: params[:price])
-        redirect to "/listings/#{@listing.agent_id}"
+        redirect to "/agents/#{@listing.agent_id}"
       else
         redirect to '/agents/show'
       end
